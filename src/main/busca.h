97843d0c45ad71039ce9b0b90ff77fe94cf1c59f
Vitor Assunção <assuncao26vitor@gmail.com>
2019-11-20 21:32:23 -0300

    Bebam água

--- src/main/busca.h
+++ src/main/busca.h
@@ -9,7 +9,7 @@
 class Busca {
     public:
         //Constrói a estrutura de busca com base no indice invertido
-        Busca(IndiceInvertido indiceInvertido, Indexador& indexador);
+        Busca(IndiceInvertido& indiceInvertido, Indexador& indexador, std::vector<Documento>& documentos);
 
         //Retorna os documentos ordenados por relevancia para a busca 'query'
         /*
@@ -23,6 +23,9 @@ class Busca {
 
     private:
         Indexador& _indexador;
+        IndiceInvertido& _indiceInvertido;
+        std::vector<Documento>& _documentos;
+        
 };
 
 #endif //TRABALHO_PRATICO_PDS_2_SRC_BUSCA_H
