6d02800d5599756d17d0f6d3bb8a42206941617c
Gustavo Couto <gustavo.3285@outlook.com>
2019-11-21 13:54:06 -0300

    fix

--- src/teste/teste_leitor.cpp
+++ src/teste/teste_leitor.cpp
@@ -1,1 +1,10 @@
 #include "doctest.h"
+#include <iostream>
+#include <vector>
+#include <map>
+#include "indice_invertido.h"
+#include "documento.h"
+
+class Teste {
+    public:
+        static Documento lerDocumento(){
